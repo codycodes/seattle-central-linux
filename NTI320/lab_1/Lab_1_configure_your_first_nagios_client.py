905af94c1dc1b76dbcbdb5a10c5d13feeb62c810
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-04-10 12:10:38 -0700

    change sed for python client

--- NTI320/lab_1/Lab_1_configure_your_first_nagios_client.py
+++ NTI320/lab_1/Lab_1_configure_your_first_nagios_client.py
@@ -7,5 +7,6 @@ if __name__ == "__main__":
     os.system('apt-get -y install nagios-plugins nagios-nrpe-server')
     os.system('cp /etc/nagios/nrpe.cfg /etc/nagios/nrpe.cfg.bak')
     internal_ip = input("Enter the internal ip address of your nagios-a server: ")
-    os.system("sed -i 's/allowed_hosts=127.0.0.1/allowed_hosts=127.0.0.1, " + internal_ip + "/g' /etc/nagios/nrpe.cfg")
+    os.system('sed -i "s/allowed_hosts=127.0.0.1/allowed_hosts=127.0.0.1, '
+              + internal_ip + '/g" /etc/nagios/nrpe.cfg')
     os.system("systemctl restart nagios-nrpe-server.service")
