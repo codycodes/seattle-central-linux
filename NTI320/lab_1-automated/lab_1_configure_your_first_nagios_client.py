3b00d6ea8faee9eaf59488b165ec341c73e0a34d
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-04-18 09:07:45 -0700

    copy from original lab-1 to begin complete automation

---
+++ NTI320/lab_1-automated/lab_1_configure_your_first_nagios_client.py
@@ -0,0 +1,12 @@
+#!/bin/python
+import os
+# configuration for client-a
+
+
+if __name__ == "__main__":
+    os.system('apt-get -y install nagios-plugins nagios-nrpe-server')
+    os.system('cp /etc/nagios/nrpe.cfg /etc/nagios/nrpe.cfg.bak')
+    internal_ip = raw_input("Enter the internal ip address of your nagios-a server: ")
+    os.system('sed -i "s/allowed_hosts=127.0.0.1/allowed_hosts=127.0.0.1, '
+              + internal_ip + '/g" /etc/nagios/nrpe.cfg')
+    os.system("systemctl restart nagios-nrpe-server.service")
