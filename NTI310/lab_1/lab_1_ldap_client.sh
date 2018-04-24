27e18577803ccdc190d8f72ce43a798c55290b2c
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-04-24 16:35:36 -0700

    Time to test if getent runs

--- NTI310/lab_1/lab_1_ldap_client.sh
+++ NTI310/lab_1/lab_1_ldap_client.sh
@@ -1,8 +1,22 @@
-DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND=noninteractive
 apt-get --yes install libpam-ldap nscd
-# TODO: add debconf stuff
 
+debconf_url='https://raw.githubusercontent.com/codycodes/Linux_at_SCC_NTI/master/NTI310/lab_1/debconf'
+wget --no-verbose -P /tmp/ $debconf_url
 
-wget -P /tmp/ 
-sed -i.bak "\;ldap://;s;$; $internal_ip_address;"  # add internal ip to debconf
+echo "Enter the internal ip address of your LDAP server: "
+read internal_ip
+sed -i.bak "\,ldap://,s,$,$internal_ip," /tmp/debconf # add internal ip to debconf
+
+while read line; do echo "$line" | debconf-set-selections; done < /tmp/debconf # read each line as input to set debconf
+
+apt-get --yes install libpam-ldap nscd # now that debconf is setup, get libpam
+
 sed -i.bak '/compat/s/$/ ldap/' /etc/nsswitch.conf # sed line to append ldap w/"compat" match
+
+systemctl reload nscd
+
+# Check LDAP server for users
+getent passwd
+
+unset DEBIAN_FRONTEND
