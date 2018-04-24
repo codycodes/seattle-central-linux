cf40b7bc045b4d1f56adc25f2a86461d0137faa8
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-04-24 16:23:37 -0700

    test if aliases work, add a bit more to client, remove symlinks

--- NTI310/lab_1/lab_1_ldap_client.sh
+++ NTI310/lab_1/lab_1_ldap_client.sh
@@ -1,2 +1,8 @@
-DEBIAN_FRONTEND=noninteractive apt-get install -y libpam-ldap nscd
+DEBIAN_FRONTEND=noninteractive
+apt-get --yes install libpam-ldap nscd
 # TODO: add debconf stuff
+
+
+wget -P /tmp/ 
+sed -i.bak "\;ldap://;s;$; $internal_ip_address;"  # add internal ip to debconf
+sed -i.bak '/compat/s/$/ ldap/' /etc/nsswitch.conf # sed line to append ldap w/"compat" match
