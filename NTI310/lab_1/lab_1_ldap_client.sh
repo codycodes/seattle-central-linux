90bf3b14f106238abbe48f38d1ff93c3df3a9eaa
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-04-24 19:58:31 -0700

    update script systemctl cmd, add new symbolic link, kill aliases

--- NTI310/lab_1/lab_1_ldap_client.sh
+++ NTI310/lab_1/lab_1_ldap_client.sh
@@ -1,22 +1,17 @@
+#!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 apt-get --yes install libpam-ldap nscd
 
 debconf_url='https://raw.githubusercontent.com/codycodes/Linux_at_SCC_NTI/master/NTI310/lab_1/debconf'
 wget --no-verbose -P /tmp/ $debconf_url
-
 echo "Enter the internal ip address of your LDAP server: "
 read internal_ip
 sed -i.bak "\,ldap://,s,$,$internal_ip," /tmp/debconf # add internal ip to debconf
-
 while read line; do echo "$line" | debconf-set-selections; done < /tmp/debconf # read each line as input to set debconf
 
-apt-get --yes install libpam-ldap nscd # now that debconf is setup, get libpam
-
+apt-get --yes install libpam-ldap nscd
 sed -i.bak '/compat/s/$/ ldap/' /etc/nsswitch.conf # sed line to append ldap w/"compat" match
-
-systemctl reload nscd
-
-# Check LDAP server for users
-getent passwd
+systemctl reload-or-restart nscd
+getent passwd | grep cgagnon # see if my user is on the ldap-a server
 
 unset DEBIAN_FRONTEND
