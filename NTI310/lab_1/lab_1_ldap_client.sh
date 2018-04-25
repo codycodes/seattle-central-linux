b21efb89d2acf9502dfd7b7c06e0f5e234ab8248
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-04-24 20:06:35 -0700

    update where install of libpam & nscd go in script

--- NTI310/lab_1/lab_1_ldap_client.sh
+++ NTI310/lab_1/lab_1_ldap_client.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-export DEBIAN_FRONTEND=noninteractive
-apt-get --yes install libpam-ldap nscd
-
 debconf_url='https://raw.githubusercontent.com/codycodes/Linux_at_SCC_NTI/master/NTI310/lab_1/debconf'
 wget --no-verbose -P /tmp/ $debconf_url
 echo "Enter the internal ip address of your LDAP server: "
@@ -9,10 +6,12 @@ read internal_ip
 sed -i.bak "\,ldap://,s,$,$internal_ip," /tmp/debconf # add internal ip to debconf
 while read line; do echo "$line" | debconf-set-selections; done < /tmp/debconf # read each line as input to set debconf
 
+export DEBIAN_FRONTEND=noninteractive
+apt-get --yes install libpam-ldap nscd
+unset DEBIAN_FRONTEND
+
 sed -i.bak '/compat/s/$/ ldap/' /etc/nsswitch.conf # sed line to append ldap w/"compat" match
 systemctl reload-or-restart nscd
 
 # verify that the client is working by seeing if my user is on the ldap-a server
 getent passwd | grep cgagnon
-
-unset DEBIAN_FRONTEND
