fc3ba9bc30758c0fc5e2f4012e12473bfe1fcd4b
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-04-25 19:41:06 -0700

    update sed statements, organize code, add conditional for username

--- NTI320/cacti_install/cacti_install.sh
+++ NTI320/cacti_install/cacti_install.sh
@@ -26,13 +26,23 @@ mysql -u root -p < cacti_auth.sql
 
 rpm -ql cacti | grep cacti.sql
 mysql -u cacti -p cacti < /usr/share/doc/cacti-1.0.4/cacti.sql
-vim /etc/cacti/db.php # manual editing here
 
-/etc/httpd/conf.d/cacti.conf # more manual editing
+echo -n "If you'd like to change the default user from 'cactiuser' to your own user, \
+please input one now. Otherwise leave this field blank: "
+read cacti_username
 
+# update username and password fields inside cacti/db.php config
+if [ -z "$cacti_username" ]; then
+    echo "No username supplied, using default 'cactiuser'"
+else
+    echo "Adding user: $cacti_username"
+    sed -i.bak "s,\$database_username = cactiuser,\$database_username = $cacti_username,g" /etc/cacti/db.php
+fi
+sed -i.bak "s,\$database_password = cactiuser,\$database_password = $cacti_password,g" /etc/cacti/db.php
+
+sed -i 's/Require host localhost/Require all granted/g' /etc/httpd/conf.d/cacti.conf
 systemctl reload httpd
-
 sed -i.bak 's/#//g' /etc/cron.d/cacti # remove cron comments; bring cacti to life
 
-sed -i.bak 's,SELINUX=enforcing,SELINUX=disabled,g' /etc/sysconfig/selinux # don't load an selinux policy on next boot
+sed -i 's,SELINUX=enforcing,SELINUX=disabled,g' /etc/sysconfig/selinux # don't load an selinux policy on next boot
 setenforce 0 # set selinux to permissive now
