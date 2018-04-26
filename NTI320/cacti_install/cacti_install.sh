c442bcac73ae1b5339779066d8d98711cf12c312
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-04-26 14:38:26 -0700

    update config after meeting w/teacher

--- NTI320/cacti_install/cacti_install.sh
+++ NTI320/cacti_install/cacti_install.sh
@@ -9,12 +9,17 @@ systemctl enable snmpd && systemctl start snmpd
 
 echo -n "Enter the password you'd like to use for MariaDB: "
 read db_password
-mysqladmin -u root password $db_password
+mysqladmin -u root --password=$db_password
 
 mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root -p mysql
 
 echo -n "Enter the password you'd like to use for cacti: "
 read cacti_password
+
+# TODO: Make custom username
+# echo -n "If you'd like to change the default user from 'cactiuser' to your own user, \
+# please input one now. Otherwise leave this field blank: "
+# read cacti_username
 
 echo "create database cacti;
 GRANT ALL ON cacti.* TO cacti@localhost IDENTIFIED BY '$cacti_password';
@@ -24,23 +29,22 @@ GRANT SELECT ON mysql.time_zone_name TO cacti@localhost;
 flush privileges;" > cacti_auth.sql
 mysql -u root -p < cacti_auth.sql
 
-rpm -ql cacti | grep cacti.sql
-mysql -u cacti -p cacti < /usr/share/doc/cacti-1.0.4/cacti.sql
+cacti_path=$(rpm -ql cacti | grep cacti.sql) # grabs the path to cacti
+mysql -u cacti -p cacti < /usr/share/doc/cacti-1.1.37/cacti.sql
 
-echo -n "If you'd like to change the default user from 'cactiuser' to your own user, \
-please input one now. Otherwise leave this field blank: "
-read cacti_username
 
-# update username and password fields inside cacti/db.php config
-if [ -z "$cacti_username" ]; then
-    echo "No username supplied, using default 'cactiuser'"
-else
-    echo "Adding user: $cacti_username"
-    sed -i.bak "s,\$database_username = cactiuser,\$database_username = $cacti_username,g" /etc/cacti/db.php
-fi
+# # update username and password fields inside cacti/db.php config
+# if [ -z "$cacti_username" ]; then
+#     echo "No username supplied, using default 'cactiuser'"
+# else
+#     echo "Adding user: $cacti_username"
+#     sed -i.bak "s,\$database_username = cactiuser,\$database_username = $cacti_username,g" /etc/cacti/db.php
+# fi
+sed -i.bak "s,\$database_username = cactiuser,\$database_username = cacti,g" /etc/cacti/db.php
 sed -i.bak "s,\$database_password = cactiuser,\$database_password = $cacti_password,g" /etc/cacti/db.php
 
 sed -i 's/Require host localhost/Require all granted/g' /etc/httpd/conf.d/cacti.conf
+sed -i 's/Allow from localhost/Allow from all/g' /etc/httpd/conf.d/cacti.conf
 systemctl reload httpd
 sed -i.bak 's/#//g' /etc/cron.d/cacti # remove cron comments; bring cacti to life
 
