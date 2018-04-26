e051931c1a85bdb3855f003da1b6889742fb69b9
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-04-26 07:53:44 -0700

    write a quick program to encapsulate questions in stars

---
+++ resources/tools/stars.py
@@ -0,0 +1,19 @@
+#!/bin/python
+
+'''
+A program which takes in a command line argument and outputs the result with
+stars at the first and last index.
+'''
+
+# TODO: make it work with sysarg
+# import sys
+
+
+def stars(string):
+    print('*' + string + '*')
+
+
+# if __name__ == "__main__":
+#     # if invoked as main program, parse arguments
+#     string = str(sys.argv[1:]).join(" ")
+#     print('*' + string + '*')
