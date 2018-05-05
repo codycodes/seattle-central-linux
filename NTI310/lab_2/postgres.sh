3fe339c63ffd72879f33e1e1cef684568d3c1589
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-05-04 20:44:32 -0700

    update line spacing in sql file

--- NTI310/lab_2/postgres.sh
+++ NTI310/lab_2/postgres.sh
@@ -16,13 +16,10 @@ echo "Please enter your new database password:"
 read db_password
 
 echo "CREATE DATABASE myproject;
-
 CREATE USER myprojectuser WITH PASSWORD '$db_password';
-
 ALTER ROLE myprojectuser SET client_encoding TO 'utf8';
 ALTER ROLE myprojectuser SET default_transaction_isolation TO 'read committed';
 ALTER ROLE myprojectuser SET timezone TO 'UTC';
-
 GRANT ALL PRIVILEGES ON DATABASE myproject TO myprojectuser;" > /tmp/myproject.sql
 # used in our script to input our newly coded db (above) as the postgres user
 sudo -i -u postgres psql -U postgres -f /tmp/myproject.sql && echo "myproject db add complete!"
