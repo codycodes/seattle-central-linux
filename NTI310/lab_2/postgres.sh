b6982661482bc51f290f6239013bef8e71dfcd5f
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-05-04 20:32:34 -0700

    update and test

--- NTI310/lab_2/postgres.sh
+++ NTI310/lab_2/postgres.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 # contains the extra packages that we need
-yum install epel-release -y
-
+yum install -y epel-release
 # postgres components
-yum install python-pip python-devel gcc postgresql-server postgresql-devel postgresql-contrib -y
+yum install -y python-pip python-devel gcc postgresql-server postgresql-devel postgresql-contrib
 
 postgresql-setup initdb
+# set md5 authentication
 sed -i.bak "s/ident/md5/g" /var/lib/pgsql/data/pg_hba.conf
 
 # start postgres & enable for start @ boot
 systemctl start postgresql && systemctl enable postgresql
 
-echo 'CREATE DATABASE myproject;
+echo "Please enter your new database password:"
+read db_password
 
-CREATE USER myprojectuser WITH PASSWORD 'password';
+echo "CREATE DATABASE myproject;
+
+CREATE USER myprojectuser WITH PASSWORD '$db_password';
 
 ALTER ROLE myprojectuser SET client_encoding TO 'utf8';
 ALTER ROLE myprojectuser SET default_transaction_isolation TO 'read committed';
 ALTER ROLE myprojectuser SET timezone TO 'UTC';
 
-GRANT ALL PRIVILEGES ON DATABASE myproject TO myprojectuser;' >> /tmp/myproject.sql
-
-
+GRANT ALL PRIVILEGES ON DATABASE myproject TO myprojectuser;" > /tmp/myproject.sql
 # used in our script to input our newly coded db (above) as the postgres user
-sudo -i -u postgres psql -U postgres -f /tmp/myproject.sql
+sudo -i -u postgres psql -U postgres -f /tmp/myproject.sql && echo "myproject db add complete!"
 
 systemctl status postgresql
