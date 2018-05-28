67819afe688d136b4f21cbf0a08535b3579b164c
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-05-28 10:01:49 -0700

    change initial prompts to enhance user experience

--- NTI310/django_postgres.sh
+++ NTI310/django_postgres.sh
@@ -4,12 +4,12 @@
 # GCP - Meant to be used in a Google Cloud Project
 
 # uses the metadata servers' dns resolver to get internal ip addresses of other hosts on the network
-echo "Please input the 'name' of your server (e.g. django-a)"
+echo "Please input the 'name' of your postgres server (e.g. postgres-a):"
 read your_server_name # stores _your_server_name_ that you want to get the ip address of
 internal_ip=$(getent hosts  $your_server_name$(echo .$(hostname -f |  cut -d "." -f2-)) | awk '{ print $1 }' ) # gets the ip address
 echo "Please input the database password of your postgresql server below:"
 read db_password
-echo "Please input the database password of your django superuser below:"
+echo "Please input the superuser password to create for django below:"
 read django_password
 
 yum install -y tree
