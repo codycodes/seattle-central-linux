cdfff0f4a9cdf6272663830b4617272a07931d69
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-06-05 15:11:20 -0700

    finish TODO

--- NTI310/mail_server/mail_server.sh
+++ NTI310/mail_server/mail_server.sh
@@ -4,11 +4,9 @@ yum update
 yum install epel-release
 yum install -y exim mailx dovecot
 
-#TODO:  Note that "wgeting" exim and dovecot config files is fine to complete the configuration. ✔️
-
 mkdir /root/SSL/mail.nti310.com -p
 cd /root/SSL/mail.nti310.com
-#TODO: provide state, city, and so on in your arguments. ✔️
+
 openssl req \
 -nodes -x509 -days 365 -sha256 \
 -subj '/C=US/ST=Washington/L=Seattle/O=Seattle Central College/CN=www.cody.codes' \
@@ -17,11 +15,7 @@ openssl req \
 cp mail.nti310.com.key mail.nti310.com.crt /etc/ssl/
 
 cp /etc/exim/exim.conf{,.orig} # create a backup of the file (not a directory) exim.conf
-
-#TODO: do the online tutorial for exim ✔️
-
 mkdir $HOME/MailDir
-
 curl "https://raw.githubusercontent.com/codycodes/Linux_at_SCC_NTI/master/NTI310/mail_server/exim_configuration" > /etc/exim/exim.conf
 
 systemctl start exim && systemctl enable exim
@@ -30,7 +24,6 @@ systemctl status exim
 useradd -m test
 echo 'test:test' | chpasswd # add password of "test" to user test
 
-# TODO: replace with your user ✔️
 echo "test" | /usr/sbin/exim -v test@nti310.com # locally test for mail delivery
 echo "test" | /usr/sbin/exim -v test@gmail.com # test remote mail delivery which should fail
 
@@ -48,8 +41,6 @@ tail /var/log/exim/main.log # follow the mail messages
 # 2018-05-30 21:54:54 1fO8tF-0000XZ-UR == person@mydomain.com R=dnslookup T=remote_smtp def
 # er (110): Connection timed out
 # 2018-05-30 21:55:11 1fO8xd-0000ZR-Ec H=mail.mydomain.com [65.254.254.54] Connection timed out
-
-# TODO: finish dovecot config ✔️
 
 cp -r /etc/dovecot/conf.d{,.orig} # recursively create a backup of the directory
 curl "https://raw.githubusercontent.com/codycodes/Linux_at_SCC_NTI/master/NTI310/mail_server/dovecot_ssl_config" > /etc/dovecot/conf.d/10-ssl.conf
