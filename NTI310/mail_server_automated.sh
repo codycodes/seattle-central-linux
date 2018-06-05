efd5caf0c13a580823858dfbefdb4733df81d01f
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-06-05 14:30:08 -0700

    begin testing exim

--- NTI310/mail_server_automated.sh
+++ NTI310/mail_server_automated.sh
@@ -4,7 +4,7 @@ yum update
 yum install epel-release
 yum install -y exim mailx dovecot
 
-#TODO:  Note that "wgeting" exim and dovecot config files is fine to complete the configuration.
+#TODO:  Note that "wgeting" exim and dovecot config files is fine to complete the configuration. ✔️
 
 mkdir /root/SSL/mail.nti310.com -p
 cd /root/SSL/mail.nti310.com
@@ -16,25 +16,26 @@ openssl req \
 
 cp mail.nti310.com.key mail.nti310.com.crt /etc/ssl/
 
-cp /etc/exim/exim.conf{,.orig}
+cp /etc/exim/exim.conf{,.orig} # create a backup of the file (not a directory) exim.conf
 
 #TODO: do the online tutorial for exim ✔️
 
 mkdir $HOME/MailDir
 
-curl https://raw.githubusercontent.com/codycodes/Linux_at_SCC_NTI/master/NTI310/exim_configuration > /etc/exim/exim.conf
+curl "https://raw.githubusercontent.com/codycodes/Linux_at_SCC_NTI/master/NTI310/exim_configuration" > /etc/exim/exim.conf
 
 systemctl start exim && systemctl enable exim
 systemctl status exim
 
-# sdiff exim.conf exim.conf.orig
+useradd -m test
+echo 'test:test' | chpasswd # add password of "test" to user test
 
-# TODO: replace with your user
-# echo "test" | /usr/sbin/exim -v codygagnon@nti310.com
+# TODO: replace with your user ✔️
+echo "test" | /usr/sbin/exim -v codygagnon@nti310.com
 
 tail /var/log/exim/main.log # follow the mail messages
 
-# check to ensure it looks like the following
+# check to ensure it looks like the following...
 
 # 018-05-30 21:52:31 1fO91D-0000aA-5q == person@localhost R=localuser T=local_delivery def
 # er (-1): maildir_format requires "directory" to be specified for the local_delivery transport
@@ -47,13 +48,13 @@ tail /var/log/exim/main.log # follow the mail messages
 # er (110): Connection timed out
 # 2018-05-30 21:55:11 1fO8xd-0000ZR-Ec H=mail.mydomain.com [65.254.254.54] Connection timed out
 
-# TODO: finish dovecot config
-cp /etc/dovecot/conf.d/10-{,.orig}
+# TODO: finish dovecot config ✔️
+cp -r /etc/dovecot/conf.d{,.orig} # recursively create a backup of the directory
 
-curl > #dovecot_ssl_config /etc/dovecot/conf.d/10-ssl.conf
-curl > # dovecat_auth_config /etc/dovecot/conf.d/10-auth.conf
-curl > # dovecat_mail_config /etc/dovecot/conf.d/10-mail.conf
-curl > # dovecat_master_config /etc/dovecot/conf.d/10-master.conf
+curl > "https://raw.githubusercontent.com/codycodes/Linux_at_SCC_NTI/master/NTI310/dovecot_ssl_config" /etc/dovecot/conf.d/10-ssl.conf
+curl > "https://raw.githubusercontent.com/codycodes/Linux_at_SCC_NTI/master/NTI310/dovecat_auth_config" /etc/dovecot/conf.d/10-auth.conf
+curl > "https://raw.githubusercontent.com/codycodes/Linux_at_SCC_NTI/master/NTI310/dovecat_mail_config" /etc/dovecot/conf.d/10-mail.conf
+curl > "https://raw.githubusercontent.com/codycodes/Linux_at_SCC_NTI/master/NTI310/dovecat_master_config" /etc/dovecot/conf.d/10-master.conf
 
 systemctl start dovecot && systemctl enable dovecot
 systemctl status dovecot
