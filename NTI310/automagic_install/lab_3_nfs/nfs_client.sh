c3c68e60981277ed517acd0243dbeff6f578e3f4
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-06-05 19:59:01 -0700

    remove unecessary code from nfs_client for automagic

--- NTI310/automagic_install/lab_3_nfs/nfs_client.sh
+++ NTI310/automagic_install/lab_3_nfs/nfs_client.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 # nfs client automation script
 apt-get install -y nfs-client
-echo -n "Please enter your ip address: "
-read ipaddress
-showmount -e $ipaddress
 mkdir /mnt/test
-echo "$ipaddress:/var/nfsshare/testing        /mnt/test       nfs     defaults 0 0" >> /etc/fstab
+echo "nfs_server_ip:/var/nfsshare/testing        /mnt/test       nfs     defaults 0 0" >> /etc/fstab
 mount -a
 cd /mnt/test/
