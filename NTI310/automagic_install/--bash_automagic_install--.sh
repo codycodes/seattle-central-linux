9f83d11d6fc1c7d2282ade4fb0341bf831cfe0ca
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-06-05 20:15:18 -0700

    add ldap client

--- NTI310/automagic_install/--bash_automagic_install--.sh
+++ NTI310/automagic_install/--bash_automagic_install--.sh
@@ -41,7 +41,15 @@ gcloud compute instances create ldap-a \
     --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_1_ldap/lab_1_ldap_server.sh"
 
 ldap_server_internal_ip=$(gcloud compute instances list | grep ldap-a | awk '{ print $4 }' | tail -1)
-echo $internal_ldap_ip
 
 echo "your ldap-a internal ip is $ldap_server_internal_ip"
 echo "your ldap-a internal ip is $ldap_server_internal_ip" >> /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/instance_internal_ip_servers.txt
+
+gsed -i "s,ldap_server_ip,$ldap_server_internal_ip" /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_1_ldap/lab_1_ldap_client.sh
+
+gcloud compute instances create nfs-client-a \
+    --zone us-west1-b \
+    --machine-type f1-micro \
+    --image-family ubuntu-1604-lts \
+    --image-project ubuntu-os-cloud \
+    --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_1_ldap/lab_1_ldap_client.sh"
