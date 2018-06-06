d780e57dfff0632a51f5475a8cd26a51d828a764
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-06-05 20:10:15 -0700

    nfs client & part of ldap server

--- NTI310/automagic_install/--bash_automagic_install--.sh
+++ NTI310/automagic_install/--bash_automagic_install--.sh
@@ -16,28 +16,32 @@ gcloud compute instances create nfs-a \
     --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_3_nfs/nfs_server.sh"
 
 nfs_server_internal_ip=$(gcloud compute instances list | grep nfs-a | awk '{ print $4 }' | tail -1)
-echo "your nfs-a internal ip is..."$nfs_server_internal_ip
+echo "your nfs-a internal ip is $nfs_server_internal_ip"
+echo "your nfs-a internal ip is $nfs_server_internal_ip" >> /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/instance_internal_ip_servers.txt
 
 # I use gsed on my mac since it's most similar to sed on Linux
 gsed -i "s,nfs_server_ip,$nfs_server_internal_ip" /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_3_nfs/nfs_client.sh
 
-
 gcloud compute instances create nfs-client-a \
+    --zone us-west1-b \
+    --machine-type f1-micro \
+    --image-family ubuntu-1604-lts \
+    --image-project ubuntu-os-cloud \
+    --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_3_nfs/nfs_client.sh"
+
+
+# LDAP Server + LDAP Client
+
+gcloud compute instances create ldap-a \
     --zone us-west1-b \
     --machine-type f1-micro \
     --image-family centos-7 \
     --image-project centos-cloud \
-    --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_3_nfs/nfs_client.sh"
+    --tags "http-server" \
+    --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_1_ldap/lab_1_ldap_server.sh"
 
-# # LDAP Server + LDAP Client
-#
-# gcloud compute instances create ldap-a \
-#     --zone us-west1-b \
-#     --machine-type f1-micro \
-#     --image-family centos-7 \
-#     --image-project centos-cloud \
-#     --tags "http-server" \
-#     --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/lab_1/lab_1_ldap_client.sh"
-#
-# internal_ldap_ip=$(gcloud compute instances list | grep ldap-a | awk '{ print $4 }' | tail -1)
-# echo $internal_ldap_ip
+ldap_server_internal_ip=$(gcloud compute instances list | grep ldap-a | awk '{ print $4 }' | tail -1)
+echo $internal_ldap_ip
+
+echo "your ldap-a internal ip is $ldap_server_internal_ip"
+echo "your ldap-a internal ip is $ldap_server_internal_ip" >> /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/instance_internal_ip_servers.txt
