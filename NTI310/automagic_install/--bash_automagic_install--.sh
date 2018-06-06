954f24c65b79e0309112f0c2ea57957e20a0a34d
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-06-05 20:34:57 -0700

    comment and test stage

--- NTI310/automagic_install/--bash_automagic_install--.sh
+++ NTI310/automagic_install/--bash_automagic_install--.sh
@@ -26,51 +26,64 @@ gcloud compute instances create nfs-client-a \
     --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_3_nfs/nfs_client.sh"
 
 
-# LDAP Server + LDAP Client
-
-gcloud compute instances create ldap-a \
-    --zone us-west1-b \
-    --machine-type f1-micro \
-    --image-family centos-7 \
-    --image-project centos-cloud \
-    --tags "http-server" \
-    --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_1_ldap/lab_1_ldap_server.sh"
-
-ldap_server_internal_ip=$(gcloud compute instances list | grep ldap-a | awk '{ print $4 }' | tail -1)
-
-echo "your ldap-a internal ip is $ldap_server_internal_ip"
-echo "your ldap-a internal ip is $ldap_server_internal_ip" >> /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/instance_internal_ip_servers.txt
-
-gsed -i "s,ldap_server_ip,$ldap_server_internal_ip" /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_1_ldap/lab_1_ldap_client.sh
-
-gcloud compute instances create ldap-client-a \
-    --zone us-west1-b \
-    --machine-type f1-micro \
-    --image-family ubuntu-1604-lts \
-    --image-project ubuntu-os-cloud \
-    --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_1_ldap/lab_1_ldap_client.sh"
-
-# Django Server + Posgres server and will resolve dependancies.
-
-gcloud compute instances create postgres-a \
-    --zone us-west1-b \
-    --machine-type f1-micro \
-    --image-family centos-7 \
-    --image-project centos-cloud \
-    --tags "http-server" \
-    --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_2_django_postgres/postgres.sh"
-
-postgres_server_internal_ip=$(gcloud compute instances list | grep postgres-a | awk '{ print $4 }' | tail -1)
-
-echo "your postgres-a internal ip is $postgres_server_internal_ip"
-echo "your postgres-a internal ip is $postgres_server_internal_ip" >> /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/instance_internal_ip_servers.txt
-
-gsed -i "s,postgres_server_ip,$postgres_server_internal_ip" /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_2_django_postgres/django.sh
-
-gcloud compute instances create django-a \
-    --zone us-west1-b \
-    --machine-type f1-micro \
-    --image-family centos-7 \
-    --image-project centos-cloud \
-    --tags "http-server" \
-    --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_2_django_postgres/django.sh"
+# # LDAP Server + LDAP Client
+#
+# gcloud compute instances create ldap-a \
+#     --zone us-west1-b \
+#     --machine-type f1-micro \
+#     --image-family centos-7 \
+#     --image-project centos-cloud \
+#     --tags "http-server" \
+#     --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_1_ldap/lab_1_ldap_server.sh"
+#
+# ldap_server_internal_ip=$(gcloud compute instances list | grep ldap-a | awk '{ print $4 }' | tail -1)
+# ldap_server_external_ip=$(gcloud compute instances list | grep ldap-a | awk '{ print $5 }' | tail -1)
+#
+# echo "your ldap-a internal ip is $ldap_server_internal_ip"
+# echo "your ldap-a internal ip is $ldap_server_internal_ip" >> /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/instance_internal_ip_servers.txt
+#
+# echo "your ldap-a external ip is $ldap_server_external_ip"
+# echo "your ldap-a external ip is $ldap_server_external_ip" >> /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/instance_external_ip_servers.txt
+#
+# gsed -i "s,ldap_server_ip,$ldap_server_internal_ip" /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_1_ldap/lab_1_ldap_client.sh
+#
+# gcloud compute instances create ldap-client-a \
+#     --zone us-west1-b \
+#     --machine-type f1-micro \
+#     --image-family ubuntu-1604-lts \
+#     --image-project ubuntu-os-cloud \
+#     --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_1_ldap/lab_1_ldap_client.sh"
+#
+# # Django Server + Posgres server and will resolve dependancies.
+#
+# gcloud compute instances create postgres-a \
+#     --zone us-west1-b \
+#     --machine-type f1-micro \
+#     --image-family centos-7 \
+#     --image-project centos-cloud \
+#     --tags "http-server" \
+#     --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_2_django_postgres/postgres.sh"
+#
+# postgres_server_internal_ip=$(gcloud compute instances list | grep postgres-a | awk '{ print $4 }' | tail -1)
+# postgres_server_external_ip=$(gcloud compute instances list | grep postgres-a | awk '{ print $5 }' | tail -1)
+#
+# echo "your postgres-a internal ip is $postgres_server_internal_ip"
+# echo "your postgres-a internal ip is $postgres_server_internal_ip" >> /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/instance_internal_ip_servers.txt
+#
+# echo "your postgres-a external ip is $postgres_server_external_ip"
+# echo "your postgres-a external ip is $postgres_server_external_ip" >> /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/instance_external_ip_servers.txt
+#
+# gsed -i "s,postgres_server_ip,$postgres_server_internal_ip" /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_2_django_postgres/django.sh
+#
+# gcloud compute instances create django-a \
+#     --zone us-west1-b \
+#     --machine-type f1-micro \
+#     --image-family centos-7 \
+#     --image-project centos-cloud \
+#     --tags "http-server" \
+#     --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_2_django_postgres/django.sh"
+#
+# django_server_external_ip=$(gcloud compute instances list | grep django-a | awk '{ print $5 }' | tail -1)
+#
+# echo "your django-a external ip is $django_server_external_ip:8000"
+# echo "your django-a external ip is $django_server_external_ip:8000" >> /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/instance_external_ip_servers.txt
