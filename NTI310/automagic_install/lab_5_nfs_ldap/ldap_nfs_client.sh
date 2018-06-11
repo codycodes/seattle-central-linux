6032c242cb1cbce267501353f43ce79ec1ec2990
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-06-11 09:19:29 -0700

    remove checking if user exists (can be done manually)

--- NTI310/automagic_install/lab_5_nfs_ldap/ldap_nfs_client.sh
+++ NTI310/automagic_install/lab_5_nfs_ldap/ldap_nfs_client.sh
@@ -11,9 +11,6 @@ unset DEBIAN_FRONTEND
 sed -i.bak '/compat/s/$/ ldap/' /etc/nsswitch.conf # sed line to append ldap w/"compat" match
 systemctl reload-or-restart nscd
 
-# verify that the client is working by seeing if my user is on the ldap-a server
-getent passwd | grep cgagnon
-
 # nfs client automation script
 apt-get install -y nfs-client
 mkdir /mnt/test
