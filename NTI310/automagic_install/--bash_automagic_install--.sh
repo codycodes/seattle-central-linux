4cb2f50c9bb9374d852115d4c00128ae8375143d
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-06-11 08:26:03 -0700

    finish ldap + nfs client creation

--- NTI310/automagic_install/--bash_automagic_install--.sh
+++ NTI310/automagic_install/--bash_automagic_install--.sh
@@ -2,31 +2,8 @@
 
 echo "Welcome! This code will launch nfs and ldap client + servers as well as Postgres and Django"
 
-# NFS Server + NFS Client
 
-gcloud compute instances create nfs-a \
-    --zone us-west1-b \
-    --machine-type f1-micro \
-    --image-family centos-7 \
-    --image-project centos-cloud \
-    --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_3_nfs/nfs_server.sh"
-
-nfs_server_internal_ip=$(gcloud compute instances list | grep nfs-a | awk '{ print $4 }' | tail -1)
-echo "your nfs-a internal ip is $nfs_server_internal_ip"
-echo "your nfs-a internal ip is $nfs_server_internal_ip" >> /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/instance_internal_ip_servers.txt
-
-# I use gsed on my mac since it's most similar to sed on Linux
-gsed -i "s,nfs_server_ip,$nfs_server_internal_ip," /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_3_nfs/nfs_client.sh
-
-gcloud compute instances create nfs-client-a \
-    --zone us-west1-b \
-    --machine-type f1-micro \
-    --image-family ubuntu-1604-lts \
-    --image-project ubuntu-os-cloud \
-    --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_3_nfs/nfs_client.sh"
-
-
-# LDAP Server + LDAP Client
+# LDAP Server
 
 gcloud compute instances create ldap-a \
     --zone us-west1-b \
@@ -45,14 +22,38 @@ echo "your ldap-a internal ip is $ldap_server_internal_ip" >> /Users/codes/__COD
 echo "your ldap-a url is $ldap_server_external_ip/phpldapadmin"
 echo "your ldap-a url is $ldap_server_external_ip/phpldapadmin" >> /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/instance_external_ip_servers.txt
 
-gsed -i "s,ldap_server_ip,$ldap_server_internal_ip," /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_1_ldap/lab_1_ldap_client.sh
 
-gcloud compute instances create ldap-client-a \
+# NFS Server
+
+gcloud compute instances create nfs-a \
+    --zone us-west1-b \
+    --machine-type f1-micro \
+    --image-family centos-7 \
+    --image-project centos-cloud \
+    --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_3_nfs/nfs_server.sh"
+
+nfs_server_internal_ip=$(gcloud compute instances list | grep nfs-a | awk '{ print $4 }' | tail -1)
+echo "your nfs-a internal ip is $nfs_server_internal_ip"
+echo "your nfs-a internal ip is $nfs_server_internal_ip" >> /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/instance_internal_ip_servers.txt
+
+# I use gsed on my mac since it's most similar to sed on Linux
+gsed -i "s,nfs_server_ip,$nfs_server_internal_ip," /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_5_nfs_ldap/ldap_nfs_client.sh
+gsed -i "s,ldap_server_ip,$ldap_server_internal_ip," /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_5_nfs_ldap/ldap_nfs_client.sh
+
+gcloud compute instances create client-a \
     --zone us-west1-b \
     --machine-type f1-micro \
     --image-family ubuntu-1604-lts \
     --image-project ubuntu-os-cloud \
-    --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_1_ldap/lab_1_ldap_client.sh"
+    --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_5_nfs_ldap/ldap_nfs_client.sh"
+
+gcloud compute instances create client-b \
+    --zone us-west1-b \
+    --machine-type f1-micro \
+    --image-family ubuntu-1604-lts \
+    --image-project ubuntu-os-cloud \
+    --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_5_nfs_ldap/ldap_nfs_client.sh"
+
 
 # Django Server + Posgres server and will resolve dependancies.
 
@@ -88,6 +89,9 @@ django_server_external_ip=$(gcloud compute instances list | grep django-a | awk 
 echo "your django-a url is $django_server_external_ip:8000/admin"
 echo "your django-a url is $django_server_external_ip:8000/admin" >> /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/instance_external_ip_servers.txt
 
+# Mail server
+
+
 gcloud compute instances create mail-a \
     --zone us-west1-b \
     --machine-type f1-micro \
@@ -95,7 +99,7 @@ gcloud compute instances create mail-a \
     --image-project centos-cloud \
     --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_4_mail_server/mail_server_automated.sh"
 
-mail_server_internal_ip=$(gcloud compute instances list | grep postgres-a | awk '{ print $4 }' | tail -1)
+mail_server_internal_ip=$(gcloud compute instances list | grep mail-a | awk '{ print $4 }' | tail -1)
 
 echo "your mail-a internal ip is $mail_server_internal_ip"
 echo "your mail-a internal ip is $mail_server_internal_ip" >> /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/instance_internal_ip_servers.txt
