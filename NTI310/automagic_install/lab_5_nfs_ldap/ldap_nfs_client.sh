a489a8394a841438e4acf5c53e4539f8cece1317
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-06-11 08:23:30 -0700

    update debconf url, add nfs client automation

--- NTI310/automagic_install/lab_5_nfs_ldap/ldap_nfs_client.sh
+++ NTI310/automagic_install/lab_5_nfs_ldap/ldap_nfs_client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-debconf_url='https://raw.githubusercontent.com/codycodes/Linux_at_SCC_NTI/master/NTI310/automagic_install/lab_1_ldap/debconf'
+debconf_url='https://raw.githubusercontent.com/codycodes/Linux_at_SCC_NTI/master/NTI310/automagic_install/ldab_5_nfs_ldap/debconf'
 wget --no-verbose -P /tmp/ $debconf_url
 sed -i.bak "\,ldap://,s,$,ldap_server_ip," /tmp/debconf # add internal ip to debconf
 while read line; do echo "$line" | debconf-set-selections; done < /tmp/debconf # read each line as input to set debconf
@@ -13,3 +13,10 @@ systemctl reload-or-restart nscd
 
 # verify that the client is working by seeing if my user is on the ldap-a server
 getent passwd | grep cgagnon
+
+# nfs client automation script
+apt-get install -y nfs-client
+mkdir /mnt/test
+echo "nfs_server_ip:/var/nfsshare/testing        /mnt/test       nfs     defaults 0 0" >> /etc/fstab
+mount -a
+cd /mnt/test/
