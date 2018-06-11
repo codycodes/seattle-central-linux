c9c09ca80767a1e12ce210578887f27290b48d86
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-06-11 08:22:12 -0700

    remove unnecessary client automation code

--- NTI310/automagic_install/lab_3_nfs/nfs_client.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# nfs client automation script
-apt-get install -y nfs-client
-mkdir /mnt/test
-echo "nfs_server_ip:/var/nfsshare/testing        /mnt/test       nfs     defaults 0 0" >> /etc/fstab
-mount -a
-cd /mnt/test/
