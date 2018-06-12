b63e11b1a5cbae2aec0166fbc4285175a1a5dd9e
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-06-12 09:16:56 -0700

    add external ip for repo-a and rpm_url

--- NTI320/automagic_install/--bash_automagic_install--.sh
+++ NTI320/automagic_install/--bash_automagic_install--.sh
@@ -3,36 +3,40 @@
 echo "Welcome to the NTI320 automagic_install script! This code will launch nfs and ldap client + servers as well as Postgres and Django and a mail server"
 
 user_filepath='/Users/codes/__CODE'
+rpm_url=''
 
 # Repo Server
+
+gsed -i "s,rpm_url,$rpm_url," $user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/lab_5_repo/repo_server.sh
 
 gcloud compute instances create repo-a \
     --zone us-west1-b \
     --machine-type f1-micro \
     --image-family centos-7 \
     --image-project centos-cloud \
-    --metadata-from-file startup-script="$user_filepath/Linux_at_SCC_NTI/NTI310/automagic_install/script"
+    --metadata-from-file startup-script="$user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/lab_5_repo/repo_server.sh"
 
 repo_server_internal_ip=$(gcloud compute instances list | grep repo-a | awk '{ print $4 }' | tail -1)
-
-ldap_server_external_ip=$(gcloud compute instances list | grep ldap-a | awk '{ print $5 }' | tail -1)
+repo_server_external_ip=$(gcloud compute instances list | grep repo-a | awk '{ print $5 }' | tail -1)
 
 echo "your repo-a internal ip is $repo_server_internal_ip"
 echo "your repo-a internal ip is $repo_server_internal_ip" >> $user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/instance_internal_ip_servers.txt
 
+echo "your repo-a external ip is http://$repo_server_external_ip"
+echo "your repo-a external ip is http://$repo_server_external_ip" >> $user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/instance_internal_ip_servers.txt
 
-# # NFS Server
-#
-# gcloud compute instances create nfs-a \
-#     --zone us-west1-b \
-#     --machine-type f1-micro \
-#     --image-family centos-7 \
-#     --image-project centos-cloud \
-#     --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_3_nfs/nfs_server.sh"
-#
-# nfs_server_internal_ip=$(gcloud compute instances list | grep nfs-a | awk '{ print $4 }' | tail -1)
-# echo "your nfs-a internal ip is $nfs_server_internal_ip"
-# echo "your nfs-a internal ip is $nfs_server_internal_ip" >> /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/instance_internal_ip_servers.txt
+
+# Build Server
+
+gcloud compute instances create build-a \
+    --zone us-west1-b \
+    --machine-type f1-micro \
+    --image-family centos-7 \
+    --image-project centos-cloud \
+    --metadata-from-file startup-script="$user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/lab_4_build/build_server.sh"
+
+gsed -i "s,repo_internal_ip,$repo_server_internal_ip," $user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/lab_4_build/build_server.sh
+
 #
 # # I use gsed on my mac since it's most similar to sed on Linux
 # gsed -i "s,nfs_server_ip,$nfs_server_internal_ip," /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_5_nfs_ldap/ldap_nfs_client.sh
