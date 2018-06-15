92acb8be1193926f9bd62bbdc7f19909c1002828
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-06-14 18:27:37 -0700

    add final code and test

--- NTI320/automagic_install/--bash_automagic_install--.sh
+++ NTI320/automagic_install/--bash_automagic_install--.sh
@@ -2,15 +2,15 @@
 
 echo "Welcome to the NTI320 automagic_install script! This code will launch nfs and ldap client + servers as well as Postgres and Django and a mail server"
 
-# bash /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/--bash_automagic_install--.sh
+bash /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/--bash_automagic_install--.sh
 
 user_filepath='/Users/codes/__CODE'
-rpm_url=''
-
-
-# Repo Server
-
-gsed -i "s,rpm_url,$rpm_url," $user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/lab_5_repo/repo_server.sh
+# rpm_url=''
+#
+#
+# # Repo Server
+#
+# gsed -i "s,rpm_url,$rpm_url," $user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/lab_5_repo/repo_server.sh
 
 gcloud compute instances create repo-a \
     --zone us-west1-b \
@@ -26,7 +26,7 @@ echo "your repo-a internal ip is $repo_server_internal_ip"
 echo "your repo-a internal ip is $repo_server_internal_ip" >> $user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/instance_internal_ip_servers.txt
 
 echo "your repo-a external ip is http://$repo_server_external_ip"
-echo "your repo-a external ip is http://$repo_server_external_ip" >> $user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/instance_internal_ip_servers.txt
+echo "your repo-a external ip is http://$repo_server_external_ip" >> $user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/instance_external_ip_servers.txt
 
 
 # Rsyslog Server
@@ -65,7 +65,7 @@ gcloud compute instances create nagios-a \
     --image-family centos-7 \
     --image-project centos-cloud \
     --tags "http-server" \
-    --metadata-from-file startup-script="$user_filepath/Linux_at_SCC_NTI/NTI310/automagic_install/lab_1_nagios/nagios_server.sh"
+    --metadata-from-file startup-script="$user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/lab_1_nagios/nagios_server.sh"
 
 nagios_server_internal_ip=$(gcloud compute instances list | grep nagios-a | awk '{ print $4 }' | tail -1)
 nagios_server_external_ip=$(gcloud compute instances list | grep nagios-a | awk '{ print $5 }' | tail -1)
@@ -73,12 +73,17 @@ nagios_server_external_ip=$(gcloud compute instances list | grep nagios-a | awk 
 echo "your nagios-a internal ip is $nagios_server_internal_ip"
 echo "your nagios-a internal ip is $nagios_server_internal_ip" >> $user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/instance_internal_ip_servers.txt
 
-echo "your nagios-a external ip is http://$nagios_server_external_ip"
-echo "your nagios-a external ip is http://$nagios_server_external_ip" >> $user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/instance_internal_ip_servers.txt
+echo "your nagios-a external ip is http://$nagios_server_external_ip/nagios"
+echo "your nagios-a external ip is http://$nagios_server_external_ip/nagios" >> $user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/instance_external_ip_servers.txt
 echo "now sleeping to allow Nagios to install gracefully"
 sleep 120
 
+
 # Cacti Server
+
+gsed -i "s,repo_internal_ip,$repo_server_internal_ip," $user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/lab_2_cacti/cacti_install.sh
+gsed -i "s,syslog_internal_ip,$syslog_server_internal_ip," $user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/lab_2_cacti/cacti_install.sh
+
 
 gcloud compute instances create cacti-a \
     --zone us-west1-b \
@@ -86,54 +91,13 @@ gcloud compute instances create cacti-a \
     --image-family centos-7 \
     --image-project centos-cloud \
     --tags "http-server" \
-    --metadata-from-file startup-script="$user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/lab_5_nfs_ldap/ldap_nfs_client.sh"
+    --metadata-from-file startup-script="$user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/lab_2_cacti/cacti_install.sh"
 
+cacti_server_internal_ip=$(gcloud compute instances list | grep cacti-a | awk '{ print $4 }' | tail -1)
+cacti_server_external_ip=$(gcloud compute instances list | grep cacti-a | awk '{ print $5 }' | tail -1)
 
-# # Django Server + Posgres server and will resolve dependancies.
-#
-# gcloud compute instances create postgres-a \
-#     --zone us-west1-b \
-#     --machine-type f1-micro \
-#     --image-family centos-7 \
-#     --image-project centos-cloud \
-#     --tags "http-server" \
-#     --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_2_django_postgres/postgres.sh"
-#
-# postgres_server_internal_ip=$(gcloud compute instances list | grep postgres-a | awk '{ print $4 }' | tail -1)
-# postgres_server_external_ip=$(gcloud compute instances list | grep postgres-a | awk '{ print $5 }' | tail -1)
-#
-# echo "your postgres-a internal ip is $postgres_server_internal_ip"
-# echo "your postgres-a internal ip is $postgres_server_internal_ip" >> /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/instance_internal_ip_servers.txt
-#
-# echo "your postgres-a url is http://$postgres_server_external_ip/phpPgAdmin"
-# echo "your postgres-a url is http://$postgres_server_external_ip/phpPgAdmin" >> /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/instance_external_ip_servers.txt
-#
-# gsed -i "s,postgres_server_ip,$postgres_server_internal_ip," /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_2_django_postgres/django.sh
-#
-# gcloud compute instances create django-a \
-#     --zone us-west1-b \
-#     --machine-type f1-micro \
-#     --image-family centos-7 \
-#     --image-project centos-cloud \
-#     --tags "http-server" \
-#     --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_2_django_postgres/django.sh"
-#
-# django_server_external_ip=$(gcloud compute instances list | grep django-a | awk '{ print $5 }' | tail -1)
-#
-# echo "your django-a url is http://$django_server_external_ip:8000/admin"
-# echo "your django-a url is http://$django_server_external_ip:8000/admin" >> /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/instance_external_ip_servers.txt
-#
-# # Mail server
-#
-#
-# gcloud compute instances create mail-a \
-#     --zone us-west1-b \
-#     --machine-type f1-micro \
-#     --image-family centos-7 \
-#     --image-project centos-cloud \
-#     --metadata-from-file startup-script="/Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/lab_4_mail_server/mail_server_automated.sh"
-#
-# mail_server_internal_ip=$(gcloud compute instances list | grep mail-a | awk '{ print $4 }' | tail -1)
-#
-# echo "your mail-a internal ip is $mail_server_internal_ip"
-# echo "your mail-a internal ip is $mail_server_internal_ip" >> /Users/codes/__CODE/Linux_at_SCC_NTI/NTI310/automagic_install/instance_internal_ip_servers.txt
+echo "your cacti-a internal ip is $cacti_server_internal_ip"
+echo "your cacti-a internal ip is $cacti_server_internal_ip" >> $user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/instance_internal_ip_servers.txt
+
+echo "your cacti-a external ip is http://$cacti_server_external_ip/cacti"
+echo "your cacti-a external ip is http://$cacti_server_external_ip/cacti" >> $user_filepath/Linux_at_SCC_NTI/NTI320/automagic_install/instance_external_ip_servers.txt
